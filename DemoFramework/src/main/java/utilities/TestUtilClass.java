07e923ea83110758075c812dd0a6d782af05a5fe
aman shukla <96472661+Amanskrillcam@users.noreply.github.com>
2022-02-10 19:30:36 +0530

    Delete DemoFramework directory

--- DemoFramework/src/main/java/utilities/TestUtilClass.java
+++
@@ -1,8 +0,0 @@
-package utilities;
-
-public class TestUtilClass
-{
-    public static long PAGE_LOAD_TIMEOUT=10000;
-    public static long IMPLICIT_WAIT=10000;
-    public static long IMPLI_WAIT=3000;
-}
